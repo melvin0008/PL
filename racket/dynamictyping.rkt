cfa19a9517626bed80eda9ffcd4d3c0d3a0e8f9a
Melvin <melvin0008@gmail.com>
2015-05-02 16:27:51 +0530

    add dynamic typing

---
+++ racket/dynamictyping.rkt
@@ -0,0 +1,10 @@
+#lang racket
+(define (sum2 xs)
+  (if (null? xs)
+      0
+      (if (number? (car xs))
+          (+ (car xs) (sum2 (cdr xs)))
+          (if( list ? (car xs))
+             (+ (sum2 (car xs)) (sum2 (cdr xs)))
+             (sum2 (cdr xs))))))
+                   
