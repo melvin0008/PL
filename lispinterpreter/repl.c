3fcd70f0874d33f0c0abf43f6890a0ffcbb80f24
Melvin <melvin0008@gmail.com>
2015-05-24 11:18:05 +0530

    add lispinterpreter

---
+++ lispinterpreter/repl.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+static char input[2048];
+
+int main(int argc, char** argv) {
+
+  puts("myLisp Version 0.0.0.0.1");
+  puts("Press Ctrl+c to Exit\n");
+
+  while (1) {
+
+    fputs("mylisp> ", stdout);
+
+    fgets(input, 2048, stdin);
+
+    printf("No you're a %s", input);
+  }
+
+  return 0;
+}
