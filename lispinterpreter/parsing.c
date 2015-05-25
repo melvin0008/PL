14a2677819c5c024fa13eda63a0136221635bc32
Melvin <melvin0008@gmail.com>
2015-05-25 14:25:47 +0530

    added parising

--- lispinterpreter/parsing.c
+++ lispinterpreter/parsing.c
@@ -29,7 +29,21 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv) {
 
-  puts("myLisp Version 0.0.0.0.1");
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expression = mpc_new("expr");
+  mpc_parser_t* Mylisp = mpc_new("mylisp");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+  "                                           \
+    number : /-?[0-9]+/; \
+    operator    : '+' | '-' | '*' | '/';           \
+    expr      : <number> | '('' <operator> <expr>+ ')'; \
+    mylisp     : /^/ <operator> <expr>+ /$/ ;                    \
+  ",
+  Number, Operator, Expression ,Mylisp);
+
+  puts("myLisp Version 0.0.0.0.2");
   puts("Press Ctrl+c to Exit\n");
 
   while (1) {
@@ -38,10 +52,21 @@ int main(int argc, char** argv) {
 
     add_history(input);
 
-    printf("No you're a %s \n", input);
-  
+    mpc_result_t r;
+
+	if (mpc_parse("<stdin>", input, Mylisp, &r)) {
+	  /* On Success Print the AST */
+	  mpc_ast_print(r.output);
+	  mpc_ast_delete(r.output);
+	} else {
+	  /* Otherwise Print the Error */
+	  mpc_err_print(r.error);
+	  mpc_err_delete(r.error);
+	}
+
     free(input);
   }
 
+  mpc_cleanup(4, Number, Operator, Expression, Mylisp);
   return 0;
 }
