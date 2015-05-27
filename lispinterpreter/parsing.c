e788342e564db0ee8287e682450f05b6e60f5b66
Melvin <melvin0008@gmail.com>
2015-05-27 23:59:33 +0530

    changed version

--- lispinterpreter/parsing.c
+++ lispinterpreter/parsing.c
@@ -517,7 +517,7 @@ int main(int argc, char** argv) {
   ",
   Number, Symbol, Sexpr,Qexpr,Expression , Mylisp);
 
-  puts("myLisp Version 0.0.0.0.5");
+  puts("myLisp Version 0.0.0.0.6");
   puts("Press Ctrl+c to Exit\n");
 
   while (1) {
